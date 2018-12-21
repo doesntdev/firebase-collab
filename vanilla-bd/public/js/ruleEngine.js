941892151c1c55b7030379c5441557b3633f9e5e
dwagner4 <dean@dfwagners.net>
2018-12-21 16:55:00 -0500

    working on islegal again

--- vanilla-bd/public/js/ruleEngine.js
+++ vanilla-bd/public/js/ruleEngine.js
@@ -62,50 +62,38 @@ function getAllJumps(match) {
 // boolean  assumes somehas already checked that to is "e"
 function isLegalMv(match, fm, to) {
   let fmcode = match.boardState[fm];
+  let y = Math.floor(fm / 4);
+  let x = fm % 4;
+  let odd = (y % 2) ? true: false;;
+  let dif = to - fm ;  // shifts odd rows
+  console.log(fmcode, x, y, odd, dif);
+
   let itsLegal = false;
-  let yfm = Math.floor(fm / 4);
-  let xfm = fm % 4;
-  let diff = to - fm - (yfm % 2);  // shifts odd rows
-  console.log(fmcode, xfm, yfm, diff);
+  let moves = {sw: false, se: false, nw: false, ne: false,}
 
-  if ( fmcode != "b" && yfm < 7 && xfm > 0 && diff == 3) {
-    itsLegal = true;
-    console.log("hit 1");
-  } else {
-    console.log("hit 1",fmcode != "b", yfm < 7, xfm > 0, diff == 3)
-  }
-  if ( fmcode != "b" && yfm < 7 && xfm < 4 && diff == 4) {
-    itsLegal = true;
-    console.log("hit 2");
-  } else {
-    console.log("hit 2",fmcode != "b", yfm < 7, xfm < 4, diff == -4)
-  }
-  if ( fmcode != "r" && yfm > 0 && xfm > 0 && diff == -5) {
-    itsLegal = true;
-    console.log("hit 3");
-  } else {
-    console.log("hit 3",fmcode != "r", yfm > 0, xfm > 0, diff == -5)
-  }
-  if ( fmcode != "r" && yfm > 0 && xfm < 4 && diff == -4) {
-    itsLegal = true;
-    console.log("hit 4");
-  } else {
-    console.log("hit 4",fmcode != "r", yfm > 0, xfm < 4, diff == -4)
-  }
+  moves.sw = fmcode != 'b' && y < 7  && (
+    (!odd && dif == 3 && x > 0) || (odd && dif == 4 )
+  )
+  moves.se = fmcode != 'b' && y < 7  && (
+    (odd && dif == 5 && x < 3) || (!odd && dif == 4 )
+  )
+  moves.nw = fmcode != 'r' && y > 0  && (
+    (!odd && dif == -5 && x > 0) || (odd && dif == -4 )
+  )
+  moves.ne = fmcode != 'r' && y > 0  && (
+    (odd && dif == -3 && x < 3) || (!odd && dif == -4 )
+  )
 
-
-  return itsLegal;
+  return moves;
 }
 
 // boolean  assumes somehas already checked that to is "e"
 function isLegalJump(match, fm, to) {
   let fmcode = match.boardState[fm];
-  let jumpcode = "red";
-  if (fmcode == 'b' || fmcode == 'B') { jumpcode = "black"}
   let yfm = Math.floor(fm / 4);
   let xfm = fm % 4;
   let diff = to - fm - (yfm % 2);  // shifts odd rows
-  console.log(fmcode, jumpcode, xfm, yfm, diff);
+  console.log(fmcode, xfm, yfm, diff);
 
   let itsLegal = [false, 32];
   let jumpPc = 'e';
@@ -117,7 +105,8 @@ function isLegalJump(match, fm, to) {
     }
     console.log("hit 1");
   } else {
-    console.log("hit 1",fmcode != "b", yfm < 7, xfm > 0, diff == 7)
+    console.log("hit 1",fmcode != "b", jumpPc, yfm < 7, xfm > 0, diff == 7)
+    console.log("yo ", (fmcode == 'r' || fmcode == 'R'), (jumpPc == 'b' || jumpPc == 'B'))
   }
   if ( fmcode != "b" && yfm < 6 && xfm < 4 && diff == 9) {
     jumpPc = match.boardState[fm + 4]
@@ -126,7 +115,8 @@ function isLegalJump(match, fm, to) {
     }
     console.log("hit 2");
   } else {
-    console.log("hit 2",fmcode != "b", yfm < 6, xfm < 4, diff == 9)
+    console.log("hit 2",fmcode != "b", jumpPc, yfm < 6, xfm < 4, diff == 9)
+    console.log("yo ", (fmcode == 'r' || fmcode == 'R'), (jumpPc == 'b' || jumpPc == 'B'))
   }
   if ( fmcode != "r" && yfm > 1 && xfm > 0 && diff == -9) {
     jumpPc = match.boardState[fm + -4]
@@ -135,7 +125,8 @@ function isLegalJump(match, fm, to) {
     }
     console.log("hit 3");
   } else {
-    console.log("hit 3",fmcode != "r", yfm >1, xfm > 0, diff == -9)
+    console.log("hit 3",fmcode != "r", jumpPc, yfm >1, xfm > 0, diff == -9)
+    console.log("yo ", (fmcode == 'b' || fmcode == 'B'), (jumpPc == 'r' || jumpPc == 'R'))
   }
   if ( fmcode != "r" && yfm > 1 && xfm < 4 && diff == -7) {
     jumpPc = match.boardState[fm + -3]
@@ -145,7 +136,8 @@ function isLegalJump(match, fm, to) {
     console.log("hit 4");
   }
   else {
-    console.log("hit 4",fmcode != "r", yfm > 1, xfm < 4, diff == -7)
+    console.log("hit 4",fmcode != "r", jumpPc, yfm > 1, xfm < 4, diff == -7)
+    console.log("yo ", (fmcode == 'b' || fmcode == 'B'), (jumpPc == 'r' || jumpPc == 'R'))
   }
 
   return itsLegal;
@@ -163,7 +155,7 @@ function doTheJump( fm, to ) {
 
 // ******************  other functions   *****************
 
-const startBoard = "eeeerrrrbbbbrrrreeeeeeeeeeeeeeee";
+const startBoard = "eeeeeeerreeeeeeeeereeeeeeeeereee"; // test red moves
 // const startBoard = "rrrrbbbbrrrreeeeeeeeeeeeeeeeeeee";
 // const startBoard = "eeeeeeeeeeeeeeeeeeeerrrrbbbbrrrr";
 
