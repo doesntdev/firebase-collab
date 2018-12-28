35d08df6a3c9838500c2186e857b3db9223cc9a9
dwagner4 <dean@dfwagners.net>
2018-12-28 11:50:29 -0500

    refactoring of rules complete

--- vanilla-bd/public/js/ruleEngine.js
+++ vanilla-bd/public/js/ruleEngine.js
@@ -51,8 +51,15 @@ function doMove ( match, fm, to )  {
   }
 
   if ( fm - to > 5 || to - fm > 5){
-    [ isLJ, captured ] = isLegalJump(match, fm, to);
-    console.log("[ isLJ, captured ]", isLJ, captured );
+    // checking for legal jump isLJ
+    let jumpList = getAllJumps(match);
+    let [ isLJ, captured ] = [ false, null ];
+    for (var i = 0; i < jumpList.length; i++) {
+      if (jumpList[i][2] == fm && jumpList[i][0] == to) {
+        [ isLJ, captured ] =  [ true, jumpList[i][1] ];
+      }
+    }
+    // console.log("[ isLJ, captured ]", isLJ, captured );
 
     // Check chain first
     if ( isLJ && ((!turn.chain) || (turn.chain && turn.chainSpace == fm))) {
@@ -64,7 +71,17 @@ function doMove ( match, fm, to )  {
       if (to > 27  && code == 'r') {code = 'R'}
       if (to < 4 && code == 'b') {code = 'B'}
       match.boardState[to] = code;
-      if ( hasJump(match, to).length == 0 ) {
+
+      let newjumpList = getAllJumps(match);
+      console.log(newjumpList)
+      let hasJump = false;
+      for (var i = 0; i < newjumpList.length; i++) {
+        if (newjumpList[i][2] == to) {
+          hasJump =  true;
+        }
+      }
+
+      if ( !hasJump ) {
         match.turn = {color: 'b', chain: false, chainSpace: null};
         if (code == 'b' || code == 'B') {match.turn.color = 'r';}
       } else {
@@ -92,7 +109,13 @@ function doMove ( match, fm, to )  {
   let jumpNum = getAllJumps(match).length;
   if (jumpNum == 0) {hasAJump = false;} else {hasAJump = true;}
 
-  let isLM = isLegalMv(match, fm, to);
+  let isLM = false;
+  let mvList = getAllMvs(match);
+  for (var i = 0; i < mvList.length; i++) {
+    if (mvList[i][1] == fm && mvList[i][0] == to) {
+      isLM = true;
+    }
+  }
   if (!hasAJump && isLM) {
     let code = match.boardState[fm];
     match.boardState[fm] = 'e';
@@ -215,102 +238,6 @@ function getAllJumps(match) {
     allJumps.push(jump);
   }
   return allJumps;
-}
-
-// returns an array of legal moves from "fm"
-function hasMove ( match, fm ) {
-  let fmcode = match.boardState[fm];
-  let y = Math.floor(fm / 4);
-  let x = fm % 4;
-  let odd = y % 2;
-  // let dif = to - fm ;  // shifts odd rows
-  console.log(fmcode, x, y, odd);
-
-  let moves = [];
-
-  let testSpace = match.boardState[ fm + 3 + odd ];
-  if (fmcode != 'b' && y < 7  && x + odd > 0 && testSpace == 'e') {
-    moves.push( fm + 3 + odd );
-    console.log("A");
-  }
-  testSpace = match.boardState[ fm + 4 + odd ];
-  if (fmcode != 'b' && y < 7  &&  x + odd < 4 && testSpace == 'e') {
-    moves.push( fm + 4 + odd );
-    console.log("B");
-  }
-  testSpace = match.boardState[ fm - 5 + odd ];
-  if (fmcode != 'r' && y > 0  && x + odd > 0 && testSpace == 'e') {
-    moves.push( fm - 5 + odd );
-    console.log("C");
-  }
-  testSpace = match.boardState[ fm - 4 + odd ];
-  if (fmcode != 'r' && y > 0  && x + odd < 3 && testSpace == 'e') {
-    moves.push( fm - 4 + odd );
-    console.log("D");
-  }
-
-  return moves;
-}
-
-// returns an array of legal jumps from "fm"
-function hasJump (match, fm) {
-  let fmcode = match.boardState[fm];
-  let targetCode = 'b';
-  if (fmcode == 'b' || fmcode == 'B') { targetCode = 'r'}
-  let y = Math.floor(fm / 4);
-  let oddOffset = (y % 2)
-  let x = fm % 4;
-  let jumps = [];
-
-  // console.log(fmcode, targetCode, x, y);
-
-  let testSpace = match.boardState[ fm + 7 ]
-  if ( fmcode != 'b' && y < 6  && testSpace == 'e' && x > 0 ){
-    captured = fm + 3 + oddOffset;
-    if (match.boardState[captured].toLowerCase() == targetCode) {
-      jumps.push([fm + 7, captured]);
-    }
-  }
-  testSpace = match.boardState[ fm + 9 ]
-  if ( fmcode != 'b' && y < 6  && testSpace == 'e' && x < 3 ){
-    captured = fm + 4 + oddOffset;
-    if (match.boardState[captured].toLowerCase() == targetCode) {
-      jumps.push([fm + 9, captured]);
-    }
-  }
-  testSpace = match.boardState[ fm - 9 ]
-  if( fmcode != 'r' && y > 1  && testSpace == 'e' && x > 0 ){
-    captured = fm - 5 + oddOffset;
-    if (match.boardState[captured].toLowerCase() == targetCode) {
-      jumps.push([fm - 9, captured]);
-    }
-  }
-  testSpace = match.boardState[ fm - 7 ]
-  if ( fmcode != 'r' && y > 1  && testSpace == 'e' && x < 3 ){
-    captured = fm - 4 + oddOffset;
-    if (match.boardState[captured].toLowerCase() == targetCode) {
-      jumps.push([fm - 7, captured]);
-    }
-  }
-
-  return jumps;
-}
-
-// boolean  assumes somehas already checked that to is "e"
-function isLegalMv(match, fm, to) {
-  let mvs = hasMove(match, fm);
-  let result = mvs.includes( to );
-  return result;
-}
-
-// boolean  assumes somehas already checked that to is "e"
-function isLegalJump(match, fm, to) {
-  let jmps = hasJump(match, fm);
-  result = [false, null];
-  for (var i = 0; i < jmps.length; i++) {
-    if ( jmps[i][0] == to ) { result = [true, jmps[i][1]] }
-  }
-  return result;
 }
 
 
